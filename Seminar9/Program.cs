0df06f1f4858d4159243bdf735fb8c3c2f8a1a78
aleksey <aleksey.n.13.06@gmail.com>
2022-04-12 14:52:45 +0300

    Д.З. Семинар 9

---
+++ Seminar9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Треугольник Паскаля
+/*
+Console.WriteLine("введите порядок степени");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < number; i++)
+{
+    for (int j = number; j > i + 1; j--)
+    {
+        Console.Write(" ");
+    }
+
+    int stepen = 1;
+
+    for (int j = 0; j <= i; j++)
+    {
+        Console.Write(stepen + " ");
+        stepen = (stepen * (i - j)) / (j + 1);
+    }
+
+    Console.WriteLine();
+}
+*/
+//===============================================================================================
